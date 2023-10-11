666178f836bac96333c6bbe34b512d03f2d96836
FlorGubiotti <florgubiotti@gmail.com>
2023-10-10 21:56:45 -0300

    Agregando JPA Queries y paginacion a Persona.

--- src/main/java/com/facu/parcial/Services/BaseService.java
+++ src/main/java/com/facu/parcial/Services/BaseService.java
@@ -12,6 +12,7 @@ public interface BaseService  <E extends BaseEntidad,ID extends Serializable> {
     public List<E> findALL() throws Exception;
 
     public Page<E> findALL(Pageable pageable) throws Exception;
+
     public E findById(ID id) throws Exception;
 
     public E save(E entity) throws Exception;
